f978fe6f677077c69109e1ef5ba45b5bd1211620
Abel Greenwald <abel@localhost.localdomain>
2017-12-27 19:24:43 -0600

    Docker Demo cf_template and bash script

---
+++ launch_stack.sh
@@ -0,0 +1,11 @@
+
+if [ $# != 1 ]; then
+	echo "usage: $0 num"
+	echo "num: the number of instances to launch"
+	exit 1
+fi
+
+aws cloudformation create-stack \
+    --stack-name Docker-Hello-World \
+    --template-body file://cf_template.yaml \
+    --parameters ParameterKey=NumInstances,ParameterValue=$1
