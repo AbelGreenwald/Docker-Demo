a2711b858da42f1d48489d85bb7abf4b78d6417b
Abel Greenwald <abel@localhost.localdomain>
2017-12-27 21:21:31 -0600

    added comments to bash script

--- launch_stack.sh
+++ launch_stack.sh
@@ -1,10 +1,18 @@
+#!/bin/bash
+#
+# Creates a CloudFormation stack with CLI command
+# Mandatory arguments:
+# 1 - Number of instances to launch
+#
 
+# Check if user entered valid number of params
 if [ $# != 1 ]; then
 	echo "usage: $0 num"
 	echo "num: Number of instances to launch"
 	exit 1
 fi
 
+# Create the stack
 aws cloudformation create-stack \
     --stack-name Docker-Hello-World \
     --template-body file://cf_template.yaml \
